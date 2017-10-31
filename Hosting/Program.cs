047f80e282a555a6232f465b431ef7325f8c5ba0
Fokiruna <alekseyyarchuk@gmail.com>
2017-10-31 14:35:21 +0300

    Add WCF service

---
+++ Hosting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Services.ClientService)))
+            {
+                host.Open();
+                Console.WriteLine(DateTime.Now.ToString());
+                host.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Services/ClientService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Services
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ClientService" в коде и файле конфигурации.
+    public class ClientService : IClientService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello" + name;
+        }
+    }
+}
